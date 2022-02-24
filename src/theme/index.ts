d3f7df5b407a029cabc76029713e677b8cd927ff
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-24 18:30:42 -0300

    feat: add new theme for application

---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#38003a',
+    TEXT: '#f2f2f2',
+    SECOND: '#e02041',
+    PRIMARY_900: '#170126',
+    PRIMARY_800: '#280540',
+    PRIMARY_700: '#3b0c59',
+    PRIMARY_600: '#963cfe',
+
+    ALERT: '#73074d',
+    BUTTON: '#00ff87'
+  },
+  FONTS: {
+    REGULAR_ARCHIVO: 'Archivo_400Regular',
+    REGULAR_UBUNTU: 'Ubuntu_400Regular',
+    BOLD_ARCHIVO: 'Archivo_700Bold',
+    BOLD_UBUNTU: 'Ubuntu_700Bold'
+  }
+}
---
+++ src/theme/style.d.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import THEME from '.'
+
+declare module 'styled-components' {
+  type ThemeType = typeof THEME;
+
+  export interface DefaultTheme extends ThemeType { }
+}
