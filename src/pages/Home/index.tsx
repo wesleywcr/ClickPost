42774ab90f9191419f9326e847b03c08d528ec8a
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-24 11:45:07 -0300

    feat::tada: Initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { Container } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>ClickPosts</Text>
+    </Container>
+  )
+}
