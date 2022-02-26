895dae7a631386ce2ba7f5b1ae7cca6ea90f4002
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-26 08:20:09 -0300

    feat: add fetching data

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,9 +1,22 @@
-import Card from '../../components/Card'
+import { useEffect, useState } from 'react'
+import Card, { Post } from '../../components/Card'
 import PageHeader from '../../components/PageHeader'
+import api from '../../services/api'
 
 import { Container, ContainerScroll } from './styles'
 
 export default function Feed() {
+  const [posts, setPosts] = useState([])
+  // const [title, setTitle] = useState('')
+  // const [body,  setBody] = useState('')
+
+  useEffect(() => {
+    api.get('posts').then(res => {
+      const data = res.data
+      setPosts(data)
+    })
+  }, [])
+
   return (
     <Container>
       <PageHeader title="Lista de produtos" />
@@ -14,11 +27,9 @@ export default function Feed() {
           paddingBottom: 24
         }}
       >
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts.map((post: Post) => {
+          return <Card key={post.id} post={post} />
+        })}
       </ContainerScroll>
     </Container>
   )
