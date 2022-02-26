d4c09dbc22d4b42d166009f22ab14dddf7917d6e
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-26 18:31:57 -0300

    feat: new component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React from 'react'
+import { TextInputProps } from 'react-native'
+
+import { Container } from './styles'
+
+export function Input({ ...rest }: TextInputProps) {
+  return <Container {...rest} />
+}
