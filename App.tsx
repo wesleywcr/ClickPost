8b3c7fccfeace40b6f1fbff8677df2750c23374e
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-26 01:14:24 -0300

    feat:add expoFonts,ThemeProvider and appStack

--- App.tsx
+++ App.tsx
@@ -1,5 +1,30 @@
-import Home from './src/pages/Home'
+import { StatusBar } from 'expo-status-bar'
+import AppStack from './src/routes/AppStack'
+import AppLoading from 'expo-app-loading'
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts
+} from '@expo-google-fonts/archivo'
+import { Ubuntu_400Regular, Ubuntu_700Bold } from '@expo-google-fonts/ubuntu'
+import { ThemeProvider } from 'styled-components/native'
+import Theme from './src/theme'
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Ubuntu_400Regular,
+    Ubuntu_700Bold
+  })
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <ThemeProvider theme={Theme}>
+        <AppStack />
+        <StatusBar style="light" />
+      </ThemeProvider>
+    )
+  }
 }
