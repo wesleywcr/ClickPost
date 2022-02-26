7ea583f13d5ef64c27dd8f6bfb8c1f312b2e7880
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-26 01:11:50 -0300

    feat: ediy  component Card and styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,49 @@
-import { Container } from './styles'
+import { Text } from 'react-native'
+import {
+  BodyContent,
+  Container,
+  ContainerButtons,
+  Button,
+  Footer,
+  Info,
+  Profile,
+  TitleContent
+} from './styles'
+import { Feather } from '@expo/vector-icons'
 
-function Card() {
-  return <Container></Container>
+export interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
 }
+interface PostProps {
+  post: Post
+}
+export default function Card({ post }: PostProps) {
+  return (
+    <Container>
+      <Profile>
+        <Info>
+          <TitleContent>{post.title}</TitleContent>
+        </Info>
+        <BodyContent>{post.body}</BodyContent>
+        <Footer>
+          <ContainerButtons>
+            <Button>
+              <Text>
+                <Feather name="edit" size={30} color="#280540" />
+              </Text>
+            </Button>
 
-export default Card
+            <Button>
+              <Text>
+                <Feather name="trash-2" size={30} color="#280540" />
+              </Text>
+            </Button>
+          </ContainerButtons>
+        </Footer>
+      </Profile>
+    </Container>
+  )
+}
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,12 +1,49 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
 
 export const Container = styled.View`
-      align-items: center;
-      width: 180px;
-      height: 250px;
-      margin: 0 10px;
-      border-radius: 10px;
+  border-width: 2px;
+  border-color: ${({ theme }) => theme.COLORS.PRIMARY_900};
+  margin-bottom: 16px;
+  border-radius: 10px;
+  overflow: hidden;
 
-      background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
-
-`;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`
+export const Profile = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 26px;
+`
+export const Info = styled.View`
+  margin-left: 18px;
+`
+export const TitleContent = styled.Text`
+  color: ${({ theme }) => theme.COLORS.TEXT};
+  font-size: 26px;
+  font-family: ${({ theme }) => theme.FONTS.BOLD_ARCHIVO};
+`
+export const BodyContent = styled.Text`
+  margin: 25px;
+  font-size: 14px;
+  line-height: 28px;
+  color: ${({ theme }) => theme.COLORS.TEXT};
+  font-family: ${({ theme }) => theme.FONTS.REGULAR_UBUNTU};
+`
+export const Footer = styled.View`
+  padding: 24px;
+  align-items: center;
+`
+export const ContainerButtons = styled.View`
+  flex-direction: row;
+  margin-top: 18px;
+`
+export const Button = styled(RectButton)`
+  width: 56px;
+  height: 56px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  background-color: ${({ theme }) => theme.COLORS.BUTTON};
+`
