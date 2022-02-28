55743dce58deec3d5ad04fe8cfd42faf4f82c50c
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-28 01:04:11 -0300

    feat: add condicional user msg

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -1,19 +1,35 @@
 import { User } from '../../screens/Profile'
 import { Content, InfoText } from './styles'
 interface InfoCardProps {
-  user: User
+  user: User | undefined
 }
 export default function InfoCard({ user }: InfoCardProps) {
   return (
     <Content>
-      <InfoText>{`Nome: ${user.name}`}</InfoText>
-      <InfoText>{`@${user.username}`}</InfoText>
-      <InfoText>{`Email: ${user.email}`}</InfoText>
-      <InfoText>{`Endereço: ${user.address.street}`}</InfoText>
-      <InfoText>{`Cidade:${user.address.city}`}</InfoText>
-      <InfoText>{`Telefone:${user.phone}`}</InfoText>
-      <InfoText>{`Website:${user.website}`}</InfoText>
-      <InfoText>{`Empresa que trabalha:${user.company.name}`}</InfoText>
+      <InfoText>{`Nome: ${
+        user ? user?.name : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`@${
+        user ? user?.username : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Email: ${
+        user ? user?.email : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Endereço: ${
+        user ? user?.address.street : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Cidade:${
+        user ? user?.address.city : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Telefone:${
+        user ? user?.phone : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Website:${
+        user ? user?.website : ' informações não encontradas'
+      }`}</InfoText>
+      <InfoText>{`Empresa que trabalha:${
+        user ? user?.company.name : ' informações não encontradas'
+      }`}</InfoText>
     </Content>
   )
 }
