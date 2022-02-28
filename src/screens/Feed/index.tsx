f264862a6d6d92a288289b9e8de2885f9544d9f3
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-28 01:06:20 -0300

    feat: add resfresh in screen

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,34 +1,51 @@
-import { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useCallback, useEffect, useState } from 'react'
+import { RefreshControl } from 'react-native'
 
 import Card, { Post } from '../../components/Card'
 import PageHeader from '../../components/PageHeader'
+import { propsStack } from '../../routes/Models'
 
 import api from '../../services/api'
 
 import { Container, ContainerScroll } from './styles'
 
 export default function Feed() {
-  const [posts, setPosts] = useState([])
+  const navigation = useNavigation<propsStack>()
 
-  // const [title, setTitle] = useState('')
-  // const [body,  setBody] = useState('')
+  const [posts, setPosts] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+  const [load, setLoad] = useState(true)
+
+  const wait = (timeout: number) => {
+    return new Promise(resolve => setTimeout(resolve, timeout))
+  }
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true)
+    wait(2000).then(() => setRefreshing(false))
+  }, [])
 
   useEffect(() => {
     api.get('posts').then(res => {
       const data = res.data
       setPosts(data)
     })
-  }, [])
+    navigation.addListener('focus', () => setLoad(!load))
+  }, [load, navigation])
 
   return (
     <Container>
-      <PageHeader title="Lista de produtos" displaybutton="flex" />
+      <PageHeader title="Lista de Posts" displaybutton="flex" />
 
       <ContainerScroll
         contentContainerStyle={{
           paddingHorizontal: 16,
           paddingBottom: 24
         }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
         {posts.map((post: Post) => {
           return <Card key={post.id} post={post} />
