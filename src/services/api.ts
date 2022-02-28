df38422558c0f08a5e8dde0f89a1151920dee499
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-02-28 01:10:46 -0300

    feat:add  baseURL one JsonServer, other jsonplace

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/'
+  baseURL: 'https://my-json-server.typicode.com/wesleywcr/fakeapi/'
 })
 
 export default api
+//api jsonplaceholder=> 'https://my-json-server.typicode.com/wesleywcr/fakeapi/'
+//api Json-Server => 'http://10.0.0.104:3333/'
